ee29c1283b6bc6763e602e06718b14a34c0617a9
Shruthi Rajashekar <shruthi@guest-wireless-207-151-057-221.usc.edu>
2015-09-11 13:51:14 -0700

    final shell scripts

--- receiver.sh
+++ receiver.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 make clean; make
-./fscp -r 
-./fscp -s -h $1 -f input.bin -ack 3 > /dev/null
+echo "Receiver!"
+echo "Receiving $2"
+./fscp -r -f $2 > /dev/null 2>&1
+echo "Sending $2 back to the sender" 
+./fscp -s -h $1 -f $2 -ack 1 > /dev/null 2>&1
